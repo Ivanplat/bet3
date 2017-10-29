0fef0662913b61362515f86a8e3f1a9b8b0d4216
Ivanplat <Platonov20030603@gmail.com>
2017-10-29 16:03:59 +0300

    22

--- index.php
+++ index.php
@@ -1,16 +1,16 @@
 <?php
 
 #подключаем системные файлы
-require_once '../system/system.config.php';
+require_once 'system/system.config.php';
 
 #подключаем БД
-require_once '../system/db.config.php';
+require_once 'system/db.config.php';
 
 #указываем title-страницы
 $title = 'Личный кабинет';
 
 #подключаем "шапку"
-require_once '../system/header.php';
+require_once 'system/header.php';
 
 #подключаем обработчик страницы
 require_once 'index/control/index.php';
@@ -19,4 +19,4 @@ require_once 'index/control/index.php';
 require_once 'index/view/index.php';
 
 #подключаем "ноги"
-require_once '../system/footer.php';
+require_once 'system/footer.php';
